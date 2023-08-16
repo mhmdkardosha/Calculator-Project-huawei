474b04b4eef0d4c31c6aca362d7ba3d0ad7012cb
Mohamed Kardosha <mohamedkrdsh9@gmail.com>
2023-08-16 15:19:42 +0300

    Adding comments

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def add(a, b):
+    # Return the sum of a and b
     return a + b
 
 
 def sub(a, b):
+    # Return the difference between a and b
     return a - b
 
 
 def mul(a, b):
+    # Return the product of a and b
     return a * b
 
 
 def div(a, b):
+    # Return the quotient of a and b
     if b == 0:
         print('Invalid equation: Division by zero')
         return None
@@ -18,6 +22,7 @@ def div(a, b):
 
 
 def mod(a, b):
+    # Return the remainder of a divided by b
     if b == 0:
         print('Invalid equation: Modulo by zero')
         return None
@@ -27,6 +32,7 @@ def mod(a, b):
 def main():
     while True:
         eq = input('Enter equation: ')
+        # Parse the equation to get the operator
         if '+' in eq:
             a, b = eq.split('+')
             answer = add(float(a), float(b))
